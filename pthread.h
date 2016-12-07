e3a786ba6b91ee76bd3f73a54265e06bcff2b581
Davee <davee@x-fusion.co.uk>
2016-12-07 06:03:04 +0000

    set minimum stack size to 32KB

--- pthread.h
+++ pthread.h
@@ -364,7 +364,7 @@ enum
 #define PTHREAD_KEYS_MAX                        _POSIX_THREAD_KEYS_MAX
 
 #undef PTHREAD_STACK_MIN
-#define PTHREAD_STACK_MIN                       0
+#define PTHREAD_STACK_MIN                       32*1024
 
 #undef _POSIX_THREAD_THREADS_MAX
 #define _POSIX_THREAD_THREADS_MAX               64
