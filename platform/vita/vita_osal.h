39a30aba3b18397f472a6c37c0ca3bb5bf01a0bf
Ivan Epifanov <isage.dna@gmail.com>
2021-06-29 00:05:59 +0300

    remove junk

--- platform/vita/vita_osal.h
+++ platform/vita/vita_osal.h
@@ -34,31 +34,5 @@ typedef SceUID pte_osSemaphoreHandle;
 
 typedef SceUID pte_osMutexHandle;
 
-
-#define OS_IS_HANDLE_VALID(x) ((x) > 0)
-
-
-
-
-#define OS_MAX_SIMUL_THREADS 10
-
-#define OS_DEFAULT_PRIO 160
-
-#define OS_MIN_PRIO 160-10
-#define OS_MAX_PRIO 160+11
-
-#define HAVE_THREAD_SAFE_ERRNO
-
 #define POLLING_DELAY_IN_us 100
-
-#define OS_MAX_SEM_VALUE 254
-
-int PspInterlockedExchange(int *ptarg, int val);
-int PspInterlockedCompareExchange(int *pdest, int exchange, int comp);
-int  PspInterlockedExchangeAdd(int volatile* pAddend, int value);
-int PspInterlockedDecrement(int *pdest);
-int PspInterlockedIncrement(int *pdest);
-
-
-
-
+#define OS_MAX_SIMUL_THREADS 10
